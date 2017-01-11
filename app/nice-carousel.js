2cad8c9622af7e6b4c320060023b001764957228
3rror404 <daniel_rochelle@Daniels-MacBook-Pro.local>
2017-01-11 09:25:41 +0000

    Adding more complex, 'nice-carousel' template

--- app/nice-carousel.js
+++ app/nice-carousel.js
@@ -1,24 +1,36 @@
+var frameModule = require("ui/frame");
 var observableModule = require("data/observable");
+var Platform = require('platform');
+
 var pageData = new observableModule.Observable();
+
 var myDataArray = [
-	{title:"Slide 1", color: "#000000", image:"~/img/enjoying-a-tasty-burger-picjumbo-com.jpg"},
-	{title:"Slide 2", color: "#000000", image:"~/img/fresh-oranges-on-small-farmers-market-picjumbo-com.jpg"},
-	{title:"Slide 3", color: "#000000", image:"~/img/knife-and-fork-salt-and-pepper-dinner-is-ready-picjumbo-com.jpg"},
-	{title:"Slide 4", color: "#000000", image:"~/img/lazy-sunday-healthy-breakfast-picjumbo-com.jpg"}
+	{title:"Slide 1", color: "rgba(0,0,0,0)", image:"~/img/enjoying-a-tasty-burger-picjumbo-com.jpg"},
+	{title:"Slide 2", color: "rgba(0,0,0,0)", image:"~/img/fresh-oranges-on-small-farmers-market-picjumbo-com.jpg"},
+	{title:"Slide 3", color: "rgba(0,0,0,0)", image:"~/img/knife-and-fork-salt-and-pepper-dinner-is-ready-picjumbo-com.jpg"},
+	{title:"Slide 4", color: "rgba(0,0,0,0)", image:"~/img/lazy-sunday-healthy-breakfast-picjumbo-com.jpg"}
 	/*{title:"Slide 5", color: "#011f4b", image: ""}*/
 ];
+
 var myCarousel = null;
 pageData.set("myDataArray", myDataArray);
 
-function pageLoaded(args) {
+exports.navigatingTo = function() {
+
+};
+
+exports.pageLoaded = function(args) {
     var page = args.object;
     page.bindingContext = pageData;
 
 	myCarousel = page.getViewById("myCarousel");
 }
-exports.pageLoaded = pageLoaded;
 
 exports.selectPageEvent = function(args){
     if(!myCarousel) return;
     myCarousel.selectedPage = 2;
+}
+
+exports.goBack = function(args) {
+	frameModule.topmost().goBack();
 }
