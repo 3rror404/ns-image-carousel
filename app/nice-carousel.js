150f8d66f87e9e2449cbf7cc185929405c946cd3
3rror404 <daniel_rochelle@Daniels-MacBook-Pro.local>
2017-01-11 12:14:23 +0000

    Add transparent ActionBar. Fades in on scroll

--- app/nice-carousel.js
+++ app/nice-carousel.js
@@ -9,7 +9,6 @@ var myDataArray = [
 	{title:"Slide 2", color: "rgba(0,0,0,0)", image:"~/img/fresh-oranges-on-small-farmers-market-picjumbo-com.jpg"},
 	{title:"Slide 3", color: "rgba(0,0,0,0)", image:"~/img/knife-and-fork-salt-and-pepper-dinner-is-ready-picjumbo-com.jpg"},
 	{title:"Slide 4", color: "rgba(0,0,0,0)", image:"~/img/lazy-sunday-healthy-breakfast-picjumbo-com.jpg"}
-	/*{title:"Slide 5", color: "#011f4b", image: ""}*/
 ];
 
 var myCarousel = null;
@@ -24,6 +23,28 @@ exports.pageLoaded = function(args) {
     page.bindingContext = pageData;
 
 	myCarousel = page.getViewById("myCarousel");
+
+	if (page.ios) {
+		var controller = frameModule.topmost().ios.controller;
+
+		/**
+		 * Make ActionBar background transparent
+		 */
+		var navBar = controller.navigationBar;
+		navBar.shadowImage = new UIImage();
+		navBar.setBackgroundImageForBarMetrics(new UIImage(), UIBarMetrics.UIBarMetricsDefault);
+
+		/**
+		 * Fade in ActionBar on scroll
+		 */
+		var scrollView = page.getViewById("scrollView");
+		scrollView.on('scroll', function(args){
+			var offset = args.object.verticalOffset;
+			//console.log((offset-50)/20);
+			navBar.backgroundColor = UIColor.colorWithRedGreenBlueAlpha(1, 0, 0, (offset-50)/20);
+		});
+	}
+	
 }
 
 exports.selectPageEvent = function(args){
